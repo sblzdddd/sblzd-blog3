70648ed370776bcf0481dfa66b6fe16c0cfc25ad
sblzdddd <101245833+sblzdddd@users.noreply.github.com>
2025-05-28 18:11:45 +0800

    initial commit

---
+++ src/config.ts
@@ -0,0 +1,73 @@
+import type {
+	LicenseConfig,
+	NavBarConfig,
+	ProfileConfig,
+	SiteConfig,
+} from "./types/config";
+import { LinkPreset } from "./types/config";
+
+export const siteConfig: SiteConfig = {
+	title: "SBLZD 3",
+	subtitle: "我是站点副标题",
+	lang: "zh_CN", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
+	themeColor: {
+		hue: 200, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+		fixed: true, // Hide the theme color picker for visitors
+	},
+	banner: {
+		enable: false,
+		src: "assets/images/demo-banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
+		credit: {
+			enable: false, // Display the credit text of the banner image
+			text: "", // Credit text to be displayed
+			url: "", // (Optional) URL link to the original artwork or artist's page
+		},
+	},
+	toc: {
+		enable: true, // Display the table of contents on the right side of the post
+		depth: 3, // Maximum heading depth to show in the table, from 1 to 3
+	},
+	favicon: [
+		// Leave this array empty to use the default favicon
+		// {
+		//   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
+		//   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
+		//   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
+		// }
+	],
+};
+
+export const navBarConfig: NavBarConfig = {
+	links: [
+		LinkPreset.Home,
+		LinkPreset.Archive,
+		LinkPreset.About,
+		LinkPreset.Friends,
+		LinkPreset.Links,
+	],
+};
+
+export const profileConfig: ProfileConfig = {
+	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	name: "SBLZD",
+	bio: "“上面一张大头照太丑了，所以我把它删了”",
+	links: [
+		{
+			name: "Bilibili",
+			icon: "fa6-brands:bilibili",
+			url: "https://space.bilibili.com/100132925",
+		},
+		{
+			name: "GitHub",
+			icon: "fa6-brands:github",
+			url: "https://github.com/sblzdddd",
+		},
+	],
+};
+
+export const licenseConfig: LicenseConfig = {
+	enable: true,
+	name: "CC BY-NC-SA 4.0",
+	url: "https://creativecommons.org/licenses/by-nc-sa/4.0/",
+};
---
+++ src/content/spec/about.md
@@ -0,0 +1,31 @@
+# 再不写博文我就是傻逼
+立了5年的flag了（猜猜标题里面的3是哪来的），啥都写不出来，我就是傻逼。
+
+
+## 你几把谁？
+
+sblzd aka. LC_022 aka. khglid. （当然应该还有别的名字。（笑
+
+男
+
+fw国际生
+
+主页写程序，副业音mad/音乐/视频/3D（都很垃圾）
+
+都说互联网没有未来，到头来网页倒是写了不少
+
+没有代表性作[二氧化硫主页](https://sulphurdxd.online/), 
+[流萤图站](https://firefly-gallery.cn)（已与牢米切割勿念）,
+以及[全世界最烂播放器](https://wbmpe.sblzd.cn)
+
+未来展望：無
+
+其他身份：东方小鬼，雀食p，饼小鬼，音游小鬼，不听术力口，chiptune，oifw，fl小鬼，
+二游p（此身份已死亡，但是欢迎出警），CKC鸡饲料代言人Since. 2020
+
+> ### 本站图片来源
+> - [Unsplash](https://unsplash.com/)
+> - [星と少女](https://www.pixiv.net/artworks/108916539) by [Stella](https://www.pixiv.net/users/93273965)
+> - [Rabbit - v1.4 Showcase](https://civitai.com/posts/586908) by [Rabbit_YourMajesty](https://civitai.com/user/Rabbit_YourMajesty)
+
+::github{repo="saicaca/fuwari"}
